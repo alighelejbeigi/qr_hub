e39452bb84533e9e710b0e84188a7c33122d66c1
alibeigi <alighb30@gmail.com>
2024-11-30 11:51:18 +0330

    add bottom nav bar to the main app

--- lib/src/pages/home_page/controller/home_page_controller.dart
+++ lib/src/pages/home_page/controller/home_page_controller.dart
@@ -9,5 +9,19 @@ class HomePageController extends GetxController {
   final HomePageRepository _repository = HomePageRepository();
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
+  // مدیریت ایندکس صفحه
+  var selectedIndex = 0.obs;
+
+  // لیست صفحات
+  final pages = [
+    const Center(child: Text('Home Page')),
+    const Center(child: Text('Search Page')),
+    const Center(child: Text('Profile Page')),
+  ];
+
+  // تغییر صفحه
+  void onTabTapped(int index) {
+    selectedIndex.value = index;
+  }
 
 }
--- lib/src/pages/home_page/view/home_page.dart
+++ lib/src/pages/home_page/view/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
-import '../../../utils/app_bar.dart';
-import '../../../utils/drawer.dart';
 import '../controller/home_page_controller.dart';
 
 class HomePage extends GetView<HomePageController> {
@@ -11,29 +8,99 @@ class HomePage extends GetView<HomePageController> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        key: controller.scaffoldKey,
-        backgroundColor: const Color(0xffF9E8D9),
-        appBar: CustomAppBar(
-          title: 'خانه',
-          onPressed: () => controller.scaffoldKey.currentState?.openDrawer(),
+        body: Obx(() => controller.pages[controller.selectedIndex.value]),
+        bottomNavigationBar: Obx(() => _buildBottomNavigationBar(context)),
+      );
+
+  Widget _buildBottomNavigationBar(BuildContext context) => Stack(
+        clipBehavior: Clip.none,
+        children: [
+          BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            notchMargin: 8,
+
+            color: const Color(0xff333333),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildBottomBarButton(
+                  context: context,
+                  label: 'History',
+                  icon: Icons.history,
+                  isSelected: controller.selectedIndex.value == 0,
+                  onTap: () => controller.onTabTapped(0),
+                ),
+                _buildBottomBarButton(
+                  context: context,
+                  label: 'Generate',
+                  icon: Icons.qr_code_2,
+                  isSelected: controller.selectedIndex.value == 1,
+                  onTap: () => controller.onTabTapped(1),
+                ),
+              ],
+            ),
+          ),
+          _buildCenterButton(context),
+        ],
+      );
+
+  Widget _buildBottomBarButton({
+    required BuildContext context,
+    required String label,
+    required IconData icon,
+    required bool isSelected,
+    required VoidCallback onTap,
+  }) =>
+      ElevatedButton(
+        onPressed: onTap,
+        style: const ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(Color(0xff333333)),
+          shadowColor: WidgetStatePropertyAll(Colors.transparent),
         ),
-       // drawer: const CustomDrawer(routeNames: RouteNames.homePage),
-        body: SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                 /* Image.asset(
-                    'assets/sample.png',
-                    package: 'holoo',
-                  ),*/
-                ],
-              )
-            ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? const Color(0xffFDB624) : Colors.white,
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? const Color(0xffFDB624) : Colors.white,
+              ),
+            ),
+            if (isSelected)
+              const DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Color(0xffFDB624),
+                ),
+                child: SizedBox(
+                  width: 70,
+                  height: 3,
+                ),
+              ),
+          ],
+        ),
+      );
+
+  Widget _buildCenterButton(BuildContext context) => Positioned(
+        bottom: 45,
+        left: MediaQuery.of(context).size.width / 2 - 40,
+        child: ElevatedButton(
+          onPressed: () => controller.onTabTapped(2), // عملکرد کلیک دکمه
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            backgroundColor: const Color(0xffFDB624),
+            padding: const EdgeInsets.all(15),
+            elevation: 5,
+          ),
+          child: Image.asset(
+            'assets/logo.png',
+            package: 'qr_hub',
+            fit: BoxFit.cover,
+            height: 40,
+            width: 40,
           ),
         ),
       );
