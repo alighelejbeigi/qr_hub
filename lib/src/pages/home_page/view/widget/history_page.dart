1c03991915b5289b0d5ee9b85d86351668dce215
alibeigi <alighb30@gmail.com>
2024-12-27 18:05:56 +0330

    update design history

--- lib/src/pages/home_page/view/widget/history_page.dart
+++ lib/src/pages/home_page/view/widget/history_page.dart
@@ -1,9 +1,7 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
-
 import '../../../../../qr_hub.dart';
 import '../../controller/home_page_controller.dart';
 
@@ -17,58 +15,73 @@ class HistoryPage extends GetView<HomePageController> {
       child: SafeArea(
         child: Column(
           children: [
-            // AppBar with TabBar
-            const Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'History',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                TabBar(
+            const SizedBox(height: 16),
+            // TabBar with a custom background, rounded borders, dark text
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff333333),
+                // Background color for the TabBar
+                borderRadius: BorderRadius.circular(12), // Rounded corners
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              // Spacing on the sides
+              child: const Padding(
+                padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                // Padding around the TabBar
+                child: TabBar(
+                  labelStyle:
+                      TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  labelColor: Color(0xffFDB624),
+                  // Color for selected tab text
+                  unselectedLabelColor: Colors.white,
+                  // Color for unselected tab text
+                  indicator: null,
+                  // Disable the bottom indicator or selection highlight
                   tabs: [
                     Tab(text: 'Scanned QR Codes'), // Tab 1
                     Tab(text: 'Generated QR Codes'), // Tab 2
                   ],
                 ),
-              ],
+              ),
             ),
             // TabBarView with content
             Expanded(
               child: TabBarView(
                 children: [
-                  // Tab 1: Show Generated QR Codes History
+                  // Tab 1: Show Scanned QR Codes History
                   _buildHistoryList<QrCodeScanHistory>(
                     future: controller.getAllHistory(),
                     itemBuilder: (context, history) {
                       return _buildHistoryTile(
-                          history, history.text, history.date, history.photo,
-                          () async {
-                        await controller.deleteHistory(history.id);
-                        if (!context.mounted) return;
-                        _showSnackBar(context, "Item deleted!");
-                        Get.forceAppUpdate();
-                      });
+                        history,
+                        history.text,
+                        history.date,
+                        history.photo,
+                        () async {
+                          await controller.deleteHistory(history.id);
+                          if (!context.mounted) return;
+                          _showSnackBar(context, "Item deleted!");
+                          Get.forceAppUpdate();
+                        },
+                      );
                     },
                   ),
-                  // Tab 2: Show Scanned QR Codes History
+                  // Tab 2: Show Generated QR Codes History
                   _buildHistoryList<QrCodeGenerateHistory>(
                     future: controller.getAllGenerationHistory(),
                     itemBuilder: (context, history) {
                       return _buildHistoryTile(
-                          history, history.text, history.date, history.photo,
-                          () async {
-                        await controller.deleteHistoryGeneration(history.id);
-                        if (!context.mounted) return;
-                        _showSnackBar(context, "Item deleted!");
-                        Get.forceAppUpdate();
-                      });
+                        history,
+                        history.text,
+                        history.date,
+                        history.photo,
+                        () async {
+                          await controller.deleteHistoryGeneration(history.id);
+                          if (!context.mounted) return;
+                          _showSnackBar(context, "Item deleted!");
+                          Get.forceAppUpdate();
+                        },
+                      );
                     },
                   ),
                 ],
@@ -119,15 +132,37 @@ class HistoryPage extends GetView<HomePageController> {
   ) {
     final formattedDate = DateFormat('yyyy/MM/dd – HH:mm:ss').format(date);
 
-    return ListTile(
-      title: Text(text),
-      subtitle: Text(formattedDate), // Display formatted date
-      leading: photo != null
-          ? Image.memory(photo, width: 50, height: 50, fit: BoxFit.cover)
-          : const Icon(Icons.image),
-      trailing: IconButton(
-        icon: const Icon(Icons.delete),
-        onPressed: onDelete,
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      color: const Color(0xff444444),
+      // Slightly lighter background for list tile
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(12),
+        title: Text(
+          text,
+          style: const TextStyle(color: Colors.white),
+        ),
+        subtitle: Text(
+          formattedDate,
+          style: const TextStyle(color: Colors.grey),
+        ),
+        leading: photo != null
+            ? ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.memory(photo,
+                    width: 50, height: 50, fit: BoxFit.cover),
+              )
+            : const Icon(
+                Icons.image,
+                color: Colors.white,
+              ),
+        trailing: IconButton(
+          icon: const Icon(Icons.delete, color: Colors.red),
+          onPressed: onDelete,
+        ),
       ),
     );
   }
